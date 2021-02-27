a9fbfe238ad04fdeaabdf0adb9f1de8c30d6ac58
Mekki Lakhdher <mlakhdher@gmail.com>
2021-02-27 17:59:45 +0100

    created the frontend crud

---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-for="(item,index) in items" :key="index">
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemchanged="$emit('reloadlist')"
+                />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from './listItem';
+import ListItem from './listItem.vue';
+
+export default {
+    props:['items'],
+    components: {
+        listItem
+    }
+}
+</script>
+
+<style scoped>
+.item {
+    background: #c6c6c6;
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
